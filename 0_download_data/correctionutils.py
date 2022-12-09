8e855e0a738477ab3aef016d803d0ca29b493dfd
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2022-12-08 17:44:04 -0700

    update data and reordered the metadata

--- 0_download_data/correctionutils.py
+++ 0_download_data/correctionutils.py
@@ -1,12 +1,14 @@
 import os
+import pathlib
 
-def run_cellprofiler(
+def correct_images(
     path_to_pipeline: str, path_to_output: str, path_to_images: str, output_folder_name : str
 ):
     """Profile batch with CellProfiler (runs segmentation and feature extraction) and rename the file after the run
     to the name of the plate
 
-    Args:
+    Parameters
+    ----------
         path_to_pipeline (str): path to the CellProfiler .cppipe file with the segmentation and feature measurement modules
         path_to_output (str): path to the output folder for the .sqlite file
         path_to_images (str): path to folder with IC images from specific plate
@@ -14,3 +16,19 @@ def run_cellprofiler(
     # run CellProfiler on a plate that has not been analyzed yet
     command = f"cellprofiler -c -r -p {path_to_pipeline} -o {path_to_output} -i {path_to_images}"
     os.system(command)
+
+def rename_images(path_to_images: pathlib.Path, output_folder_name: str):
+    """
+    reorder and rename all images 
+
+    Parameters
+    ----------
+    path_to_images : pathlib.Path
+        path to where the corrected images from CellProfiler are located
+    output_folder_name:
+        name of the folder with the outputed 
+    """
+
+    for images in path_to_images.iterdir():
+        image_names = images.name.split("_")
+        print(image_names)
