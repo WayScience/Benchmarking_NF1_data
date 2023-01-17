3137e542cb4df1f635860919b1d03c6b40c9a68b
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-01-17 15:33:49 -0700

    deeprofiler project processing features

--- 4_processing_features/4.extract_sc_features.sh
+++ 4_processing_features/4.extract_sc_features.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-jupyter nbconvert --to python extract_single_cell_features.ipynb
-python extract_single_cell_features.py
+jupyter nbconvert --to python extract_sc_features_cp.ipynb
+python extract_sc_features_cp.py
+
+jupyter nbconvert --to python extract_sc_features_dp.ipynb
+python extract_sc_features_dp.py
