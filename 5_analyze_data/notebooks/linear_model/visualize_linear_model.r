343758d46a2d9acccf6628b53bc5a879bb38613d
Gregory Way <gregory.way@gmail.com>
2023-01-19 17:01:58 -0700

    add DP visualization for linear model

--- 5_analyze_data/notebooks/linear_model/visualize_linear_model.r
+++ 5_analyze_data/notebooks/linear_model/visualize_linear_model.r
@@ -1,12 +1,15 @@
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(dplyr))
 
-output_fig_dir <- "figures"
+input_dir <- "results"
+lm_file <- file.path(input_dir, "linear_model_cp_features.tsv")
+lm_dp_file <- file.path(input_dir, "linear_model_dp_features.tsv")
 
+output_fig_dir <- "figures"
 lm_fig <- file.path(output_fig_dir, "linear_model_cp_features.png")
+lm_dp_fig <- file.path(output_fig_dir, "linear_model_dp_features.png")
 
 # Load and process linear model data
-lm_file <- file.path("results", "linear_model_cp_features.tsv")
 lm_df <- readr::read_tsv(
     lm_file, col_types = readr::cols(.default = "d", feature = "c")
 )
@@ -56,10 +59,37 @@ lm_fig_gg <- (
     )
     + ylab("WT genotype contribution (LM beta coefficient)")
     + xlab("Cell count contribution (LM beta coefficient)")
-    + ggtitle("How features contribute to NF1 genotype and cell density")
+    + ggtitle("How CellProfiler features contribute\nto NF1 genotype and cell density")
 )
-
-lm_fig_gg
 
 # Output figure
 ggsave(lm_fig, lm_fig_gg, dpi = 500, height = 6, width = 6)
+
+lm_fig_gg
+
+# Load and process linear model data
+lm_dp_df <- readr::read_tsv(
+    lm_dp_file, col_types = readr::cols(.default = "d", feature = "c")
+)
+
+print(dim(lm_dp_df))
+head(lm_dp_df, 10)
+
+lm_dp_fig_gg <- (
+    ggplot(lm_dp_df, aes(x = cell_count_coef, y = WT_coef))
+    + geom_density_2d_filled(alpha = 0.5, show.legend = FALSE)
+    + geom_point(aes(size = r2_score), alpha = 0.3)
+    + geom_vline(xintercept = 0, linetype = "dashed", color = "red")
+    + theme_bw()
+    + guides(
+        size = guide_legend(title = "R2 score")
+    )
+    + ylab("WT genotype contribution (LM beta coefficient)")
+    + xlab("Cell count contribution (LM beta coefficient)")
+    + ggtitle("How DeepProfiler features contribute\nto NF1 genotype and cell density")
+)
+
+# Output figure
+ggsave(lm_dp_fig, lm_dp_fig_gg, dpi = 500, height = 6, width = 6)
+
+lm_dp_fig_gg
