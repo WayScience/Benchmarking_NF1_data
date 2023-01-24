6b917a4dcd845ba1be626ab164e7ef50a6e07828
jenna-tomkinson <jenna.tomkinson@ucdenver.edu>
2023-01-24 13:25:07 -0700

    run all plates with cellprofiler pipelines

--- 4_processing_features/4.extract_sc_features.sh
+++ 4_processing_features/4.extract_sc_features.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 jupyter nbconvert --to python *.ipynb
 
-python extract_sc_features_cp.py
-
-python extract_sc_features_dp.py
+python extract_sc_features_cp_plate1.py
+python extract_sc_features_dp_plate1.py
+python extract_sc_features_cp_plate2.py
